8d0dc74e1469374d3cbb5179feff10324b62f7c6
Raphaelle Paquet <68092222+raphpaquet@users.noreply.github.com>
2021-03-30 20:46:56 -0400

    signin signout done

--- backend/server.js
+++ backend/server.js
@@ -2,20 +2,27 @@ const express = require("express");
 const mongoose = require("mongoose");
 const userRouter = require("./routers/userRouter.js");
 const productRouter = require("./routers/productRouter.js");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
+
+// add middlewares for all request that contains data to be translated in req.body in our node app. :
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 mongoose.connect(
   process.env.MONGODB_URL || "mongodb://localhost/petitsfruits",
   {
     useNewUrlParser: true,
-    userUnifiedTopology: true,
+    useUnifiedTopology: true,
     useCreateIndex: true,
   }
 );
 
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
-
 
 app.get("/", (req, res) => {
   res.send("Server is ready");
