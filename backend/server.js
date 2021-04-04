08e9611a7ff9cf1c712461eda54523d30ba76dcb
Raphaelle Paquet <68092222+raphpaquet@users.noreply.github.com>
2021-04-04 17:22:23 -0400

    update version npm node

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,15 @@ const orderRouter = require("./routers/orderRouter.js");
 const uploadRouter = require("./routers/uploadRouter.js");
 const path = require("path");
 
+// import express from 'express';
+// import mongoose from "mongoose";
+// import userRouter from "./routers/userRouter.js";
+// import productRouter from "./routers/productRouter.js";
+// import dotenv from "dotenv";
+// import orderRouter from "./routers/orderRouter.js";
+// import uploadRouter from "./routers/uploadRouter.js";
+// import path from "path";
+
 dotenv.config();
 
 const app = express();
