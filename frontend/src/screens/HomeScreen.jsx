07256938b410d8c5f96bb233bd7c4753f744875d
Raphaelle Paquet <68092222+raphpaquet@users.noreply.github.com>
2021-04-08 15:00:51 -0400

    sized skate

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -5,7 +5,7 @@ export default function HomeScreen() {
   return (
     <div>
       <div>
-        <img src="./images/skate.png" alt="skateboard peint par petitsfruits" className="large" />
+        <img src="./images/skate.png" alt="skateboard peint par petitsfruits" className="large" style={{maxHeight:"60rem"}}/>
       </div>
       <div>
         <Contact />
